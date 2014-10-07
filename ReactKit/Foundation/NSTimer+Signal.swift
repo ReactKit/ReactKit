bf22972142b5c790ac169bb512c0a1252f894d9d
Yasuhiro Inami <inamiy@gmail.com>
2014-10-07 22:34:31 +0900

    Add NSTimer+Signal.

---
+++ ReactKit/Foundation/NSTimer+Signal.swift
@@ -0,0 +1,43 @@
+//
+//  NSTimer+Signal.swift
+//  ReactKit
+//
+//  Created by Yasuhiro Inami on 2014/10/07.
+//  Copyright (c) 2014年 Yasuhiro Inami. All rights reserved.
+//
+
+import Foundation
+
+public extension NSTimer
+{
+    public class func signal<T>(#timeInterval: NSTimeInterval, userInfo: AnyObject? = nil, repeats: Bool = true, map: NSTimer? -> T) -> Signal<T>
+    {
+        return Signal(name: "\(NSStringFromClass(self))") { progress, fulfill, reject, configure in
+            
+            let target = _TargetActionProxy { (self_: AnyObject?) in
+                progress(map(self_ as? NSTimer))
+            }
+            
+            var timer: NSTimer?
+            
+            let createTimer: Void -> NSTimer = {
+                return NSTimer.scheduledTimerWithTimeInterval(timeInterval, target: target, selector: "_fire:", userInfo: userInfo, repeats: repeats)
+            }
+            
+            timer = createTimer()
+            
+            configure.pause = {
+                timer?.invalidate()
+                timer = nil
+            }
+            configure.resume = {
+                timer = createTimer()
+            }
+            configure.cancel = {
+                timer?.invalidate()
+                timer = nil
+            }
+            
+        }
+    }
+}
